32ca34b88ec622622c8a9a7f4627f0dfcd8f1d85
My Random Thoughts <support@myrandomthoughts.co.uk>
2017-11-01 15:48:05 +0000

    Added ignore for Domain Controllers

--- checks/compliance/com-12-only-one-server-role.ps1
+++ checks/compliance/com-12-only-one-server-role.ps1
@@ -39,24 +39,34 @@ Function com-12-only-one-server-role
 
     Try
     {
-        [string]$checkOS = (Get-CimInstance -ClassName 'Win32_OperatingSystem' -Property 'Caption' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty 'Caption')
-        If ($checkOS -like '*server*')
+        If ((Check-IsDomainController) -eq $false)
         {
-            Import-Module -Name 'ServerManager'                                                                   # Windows 2008                 Windows 2012+
-            [System.Collections.ArrayList]$gWinFe  = @(Get-WindowsFeature | Where-Object { ($_.Depth -eq 1) -and (($_.Installed -eq $true) -or ($_.InstallState -eq 'Installed')) } -ErrorAction Stop | Select-Object 'Name', 'DisplayName')
-            [System.Collections.ArrayList]$gWinFeC = $gWinFe.Clone()
+            [string]$checkOS = (Get-CimInstance -ClassName 'Win32_OperatingSystem' -Property 'Caption' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty 'Caption')
+            If ($checkOS -like '*server*')
+            {
+                Import-Module -Name 'ServerManager'                                                                   # Windows 2008                 Windows 2012+
+                [System.Collections.ArrayList]$gWinFe  = @(Get-WindowsFeature | Where-Object { ($_.Depth -eq 1) -and (($_.Installed -eq $true) -or ($_.InstallState -eq 'Installed')) } -ErrorAction Stop | Select-Object 'Name', 'DisplayName')
+                [System.Collections.ArrayList]$gWinFeC = $gWinFe.Clone()
 
-            # These are installed by default on all 2008 R2 and above servers and can be ignored
-            [System.Collections.ArrayList]$ignoreList =  ('NET-Framework-Features', 'NET-Framework', 'NET-Framework-45-Features', 'FileAndStorage-Services', 'Multipath-IO', 'RSAT', 'FS-SMB1',
-                                                          'Telnet-Client', 'User-Interfaces-Infra', 'PowerShellRoot', 'PowerShell-ISE', 'Windows-Defender-Features', 'WoW64-Support')
+                # These are installed by default on all 2008 R2 and above servers and can be ignored
+                [System.Collections.ArrayList]$ignoreList =  ('NET-Framework-Features', 'NET-Framework', 'NET-Framework-45-Features', 'FileAndStorage-Services', 'Multipath-IO', 'RSAT', 'FS-SMB1',
+                                                              'Telnet-Client', 'User-Interfaces-Infra', 'PowerShellRoot', 'PowerShell-ISE', 'Windows-Defender-Features', 'WoW64-Support')
 
-            $ignoreList                             | ForEach-Object -Process { [string]$LookingFor = $_; $gWinFe | ForEach-Object -Process { If ($_.Name -eq $LookingFor) { [void]$gWinFeC.Remove($_) } } }
-            $script:chkValues['IgnoreTheseRoles'] | ForEach-Object -Process { [string]$LookingFor = $_; $gWinFe | ForEach-Object -Process { If ($_.Name -eq $LookingFor) { [void]$gWinFeC.Remove($_) } } }
+                $ignoreList                             | ForEach-Object -Process { [string]$LookingFor = $_; $gWinFe | ForEach-Object -Process { If ($_.Name -eq $LookingFor) { [void]$gWinFeC.Remove($_) } } }
+                $script:chkValues['IgnoreTheseRoles'] | ForEach-Object -Process { [string]$LookingFor = $_; $gWinFe | ForEach-Object -Process { If ($_.Name -eq $LookingFor) { [void]$gWinFeC.Remove($_) } } }
+            }
+            Else
+            {
+                $result.result  = $script:lang['Not-Applicable']
+                $result.message = $script:lang['n001']
+                $result.data    = $checkOS
+                Return $result
+            }
         }
         Else
         {
             $result.result  = $script:lang['Not-Applicable']
-            $result.message = $script:lang['n001']
+            $result.message = $script:lang['n002']
             $result.data    = $checkOS
             Return $result
         }
