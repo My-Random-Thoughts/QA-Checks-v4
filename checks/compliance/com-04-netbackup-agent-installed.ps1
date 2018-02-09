4a1c8c65dd6d8077533bd07dcdcf87a769162bab
My Random Thoughts <support@myrandomthoughts.co.uk>
2018-02-09 16:32:20 +0000

    Missing Try / Catch

--- checks/compliance/com-04-netbackup-agent-installed.ps1
+++ checks/compliance/com-04-netbackup-agent-installed.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
     DESCRIPTION: 
         Check NetBackup agent is installed and that the correct port is open to the management server.
         Only applies to physical servers, or virtual servers with a list of known software installed.
@@ -47,62 +47,71 @@ Function com-04-netbackup-agent-installed
     
     #... CHECK STARTS HERE ...#
 
-    [object]$verCheck = (Check-Software -DisplayName $script:chkValues['ProductName'])
-    If ($verCheck -eq '-1') { Throw $script:lang['trw1'] }
-
-    If ([string]::IsNullOrEmpty($verCheck) -eq $false)
+    Try
     {
-        $result.result  =    $script:lang['Pass']
-        $result.message =    $script:lang['dt01']
-        $result.data    = ($($script:lang['dt02']) -f $verCheck.Version)
+        [object]$verCheck = (Check-Software -DisplayName $script:chkValues['ProductName'])
+        If ($verCheck -eq '-1') { Throw $script:lang['trw1'] }
 
-        [string[]]$ServerNames = @(Get-ItemProperty -Path 'HKLM:\SOFTWARE\Veritas\NetBackup\CurrentVersion\Config' -Name 'Server' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty 'Server')
-        $ServerNames | ForEach-Object -Process {
-            [boolean]$portTest = (Check-IsPortOpen -DestinationServer $_ -Port 1556)
-            If   ($portTest -eq $true)                   { $result.data += ($($script:lang['dt03']) -f $_.ToLower()) }
-            Else { $result.result = $script:lang['Fail'];  $result.data += ($($script:lang['dt04']) -f $_.ToLower()) }
-        }
-    }
-    Else
-    {
-        If ((Check-IsVMwareGuest -eq $true) -or (Check-IsHyperVGuest -eq $true))
+        If ([string]::IsNullOrEmpty($verCheck) -eq $false)
         {
-            # If backup software not installed, and is a VM, then check for additional software to see if it should be installed
-            $found = $false
-            $script:chkValues['RequiredServerRoles'] | ForEach-Object -Process {
-                [string]$verExist = (Check-Software -DisplayName $_)
-                If ($verCheck -eq '-1') { Throw $($script:lang['trw1']) }
-                If ([string]::IsNullOrEmpty($verCheck) -eq $false)
-                {
-                    $result.result  =    $script:lang['Fail']
-                    $result.message =    $script:lang['dt05']
-                    $result.data    = ($($script:lang['dt06']) -f $_)
-                    $found          =    $true
-                }
-            }
+            $result.result  =    $script:lang['Pass']
+            $result.message =    $script:lang['dt01']
+            $result.data    = ($($script:lang['dt02']) -f $verCheck.Version)
 
-            If (Check-IsDomainController -eq $true)
-            {
-                $result.result  = $script:lang['Fail']
-                $result.message = $script:lang['dt05']
-                $result.data    = $script:lang['dt07']
-                $found          = $true
-            }
-
-            If ($found -eq $false)
-            {
-                $result.result  = $script:lang['Manual']
-                $result.message = $script:lang['m001']
-                $result.data    = $script:lang['dt08']
+            [string[]]$ServerNames = @(Get-ItemProperty -Path 'HKLM:\SOFTWARE\Veritas\NetBackup\CurrentVersion\Config' -Name 'Server' -ErrorAction SilentlyContinue | Select-Object -ExpandProperty 'Server')
+            $ServerNames | ForEach-Object -Process {
+                [boolean]$portTest = (Check-IsPortOpen -DestinationServer $_ -Port 1556)
+                If   ($portTest -eq $true)                   { $result.data += ($($script:lang['dt03']) -f $_.ToLower()) }
+                Else { $result.result = $script:lang['Fail'];  $result.data += ($($script:lang['dt04']) -f $_.ToLower()) }
             }
         }
         Else
         {
-            # Physical server
-            $result.result  = $script:lang['Fail']
-            $result.message = $script:lang['dt05']
-            $result.data    = $script:lang['dt09']
+            If ((Check-IsVMwareGuest -eq $true) -or (Check-IsHyperVGuest -eq $true))
+            {
+                # If backup software not installed, and is a VM, then check for additional software to see if it should be installed
+                $found = $false
+                $script:chkValues['RequiredServerRoles'] | ForEach-Object -Process {
+                    [string]$verExist = (Check-Software -DisplayName $_)
+                    If ($verCheck -eq '-1') { Throw $($script:lang['trw1']) }
+                    If ([string]::IsNullOrEmpty($verCheck) -eq $false)
+                    {
+                        $result.result  =    $script:lang['Fail']
+                        $result.message =    $script:lang['dt05']
+                        $result.data    = ($($script:lang['dt06']) -f $_)
+                        $found          =    $true
+                    }
+                }
+
+                If (Check-IsDomainController -eq $true)
+                {
+                    $result.result  = $script:lang['Fail']
+                    $result.message = $script:lang['dt05']
+                    $result.data    = $script:lang['dt07']
+                    $found          = $true
+                }
+
+                If ($found -eq $false)
+                {
+                    $result.result  = $script:lang['Manual']
+                    $result.message = $script:lang['m001']
+                    $result.data    = $script:lang['dt08']
+                }
+            }
+            Else
+            {
+                # Physical server
+                $result.result  = $script:lang['Fail']
+                $result.message = $script:lang['dt05']
+                $result.data    = $script:lang['dt09']
+            }
         }
+    }
+    Catch
+    {
+        $result.result  = $script:lang['Error']
+        $result.message = $script:lang['Script-Error']
+        $result.data    = $_.Exception.Message
     }
     
     Return $result
